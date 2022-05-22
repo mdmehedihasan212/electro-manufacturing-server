bf18fc104089835f1ba61c9b10a10911184c85c5
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-22 14:02:05 +0600

    add tools get api for load all tools

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kh827.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db("toolsCollection").collection("tools");
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const tools = await toolsCollection.find(query).toArray();
+            res.send(tools);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Electro Manufacturing!')
