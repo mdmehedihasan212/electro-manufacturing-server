0cbee759082c9914cbf26d8ce85b6c9c177c5ef6
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-24 02:56:45 +0600

    add new orders collection for post new order

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Electro Manufacturing!')
+})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kh827.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,18 +22,26 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("toolsCollection").collection("tools");
+        const orderCollection = client.db("toolsCollection").collection("orders");
 
         app.get('/tools', async (req, res) => {
             const query = {};
-            const tools = await toolsCollection.find(query).toArray();
+            const cursor = toolsCollection.find(query)
+            const tools = await cursor.toArray();
             res.send(tools);
         })
 
         app.get('/tools/:id', async (req, res) => {
             const id = req.params.id;
-            const toolId = { _id: ObjectId(id) };
-            const tool = await toolsCollection?.findOne(toolId);
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
             res.send(tool);
+        })
+
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const purchasesOrder = await orderCollection.insertOne(orders);
+            res.send(purchasesOrder)
         })
 
     } finally {
@@ -37,11 +49,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-    res.send('Electro Manufacturing!')
-})
 
 app.listen(port, () => {
     console.log(`Example Electro Manufacturing listening on port ${port}`)
