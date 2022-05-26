b5d6a1236e3a11ad07bd5eb649ce623a82204eeb
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-26 13:43:15 +0600

    create admin role api in exist users

--- index.js
+++ index.js
@@ -60,6 +60,16 @@ async function run() {
             res.send(userReview)
         })
 
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' }
+            };
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
